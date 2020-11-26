bdcbcba0d6d91fef2851e0e056906148039febb7
Philipp Seeger <philippbseeger@googlemail.com>
2020-11-26 23:41:32 +0100

    make wandbox compilation multithread async

--- src/main/java/io/horrorshow/discordcodeformatter/DiscordCodeFormatterApplication.java
+++ src/main/java/io/horrorshow/discordcodeformatter/DiscordCodeFormatterApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.Executor;
+
 @SpringBootApplication
+@EnableAsync(proxyTargetClass = true)
 public class DiscordCodeFormatterApplication {
 
     public static void main(String[] args) {
@@ -18,4 +23,14 @@ public class DiscordCodeFormatterApplication {
         return builder.build();
     }
 
+    @Bean
+    public Executor asyncExecutor() {
+        var executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(4);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Thread-");
+        executor.initialize();
+        return executor;
+    }
 }
