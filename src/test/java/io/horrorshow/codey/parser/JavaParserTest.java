464d228400cc73e64ab74900f4db9591ee5e2011
Philipp Seeger <pseeger@k15t.com>
2021-09-04 19:16:04 +0200

    Add basic sample for parsing expressions and wrap in class with main

--- src/test/java/io/horrorshow/codey/parser/JavaParserTest.java
+++ src/test/java/io/horrorshow/codey/parser/JavaParserTest.java
@@ -1,6 +1,11 @@
 package io.horrorshow.codey.parser;
 
 import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.printer.DefaultPrettyPrinter;
 import com.github.javaparser.printer.configuration.DefaultConfigurationOption;
 import com.github.javaparser.printer.configuration.DefaultPrinterConfiguration;
@@ -32,4 +37,27 @@ public class JavaParserTest {
                 """);
     }
 
+
+    @Test
+    void wrap_expression_in_class_with_main() {
+
+        var source = "System.out.println(\"Lonely println statement\")";
+
+        var expression = StaticJavaParser.parseExpression(source);
+        var body = new BlockStmt().addStatement(expression);
+        var stringArgs = new Parameter().setType("String[]").setName("args");
+        var myClass = new CompilationUnit().addClass("CodeyClass").setPublic(true);
+        myClass.addMethod("main", Modifier.Keyword.STATIC)
+                .setStatic(true).setPublic(true)
+                .setParameters(NodeList.nodeList(stringArgs))
+                .setBody(body);
+
+        assertThat(myClass.toString()).isEqualTo("""
+                public class CodeyClass {
+                                
+                    static public void main(String[] args) {
+                        System.out.println("Lonely println statement");
+                    }
+                }""");
+    }
 }
