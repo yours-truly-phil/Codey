eb1349c502017ba8479557428bc5bf79ed1e2252
Philipp Seeger <pseeger@k15t.com>
2021-09-21 22:19:15 +0200

    Use Files api instead of loading resource as stream

--- src/test/java/io/horrorshow/codey/api/github/model/GithubApiTypesTest.java
+++ src/test/java/io/horrorshow/codey/api/github/model/GithubApiTypesTest.java
@@ -1,11 +1,11 @@
 package io.horrorshow.codey.api.github.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.micrometer.core.instrument.util.IOUtils;
 import org.junit.jupiter.api.Test;
 
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 class GithubApiTypesTest {
@@ -14,36 +14,38 @@ class GithubApiTypesTest {
     private static final String WEBHOOK_SAMPLES_DIR = "/github-webhook-samples";
 
 
-    private String getGitHubSampleFile(String file) {
-        return IOUtils.toString(this.getClass()
-                .getResourceAsStream("%s/%s".formatted(WEBHOOK_SAMPLES_DIR, file)), StandardCharsets.UTF_8);
+    private String getGitHubSampleFile(String file) throws IOException {
+        var path = Paths.get("src", "test", "resources", "%s/%s".formatted(WEBHOOK_SAMPLES_DIR, file));
+        return Files.readString(path);
     }
 
 
-    private <T> void deserialize(String json, Class<T> clazz) throws JsonProcessingException {
+    private <T> void deserialize(String json, Class<T> clazz) throws IOException {
         var payload = getGitHubSampleFile(json);
         objectMapper.readValue(payload, clazz);
     }
 
 
     @Test
-    void deserialize_sample_push_payload() throws JsonProcessingException {
+    void deserialize_sample_push_payload() throws IOException {
         deserialize("push.json", GithubApiTypes.Push.class);
     }
 
 
     @Test
-    void deserialize_sample_check_run_payload() throws JsonProcessingException {
+    void deserialize_sample_check_run_payload() throws IOException {
         deserialize("check_run.json", GithubApiTypes.CheckRunPayload.class);
     }
 
+
     @Test
-    void deserialize_workflow_run_payload() throws JsonProcessingException {
+    void deserialize_workflow_run_payload() throws IOException {
         deserialize("workflow_run.json", GithubApiTypes.WorkflowRunPayload.class);
     }
 
+
     @Test
-    void deserialize_check_suite_payload() throws JsonProcessingException {
+    void deserialize_check_suite_payload() throws IOException {
         deserialize("check_suite.json", GithubApiTypes.CheckSuitePayload.class);
     }
 }
